1500be3f65985ea4768901ba73969d250956c8a3
Jason Hudgins <jason@hudgins.co>
2024-03-10 16:45:08 -0500

    lots of refactoring, as I move api code into logical modules

--- coinmarketcap/core.py
+++ coinmarketcap/core.py
@@ -4,67 +4,12 @@ import json
 import requests
 import tempfile
 import requests_cache
-from typing import List, Optional
-from enum import Enum
-from dataclasses import dataclass
+from typing import List
 
 from .types.token_state import TokenState, Quote
 from .v2.cryptocurrency.quotes.historical import _quotes_historical_v2
-
-@dataclass
-class FilterOptions:
-    price_min: Optional[float] = None
-    price_max: Optional[float] = None
-    market_cap_min: Optional[float] = None
-    market_cap_max: Optional[float] = None
-    volume_24h_min: Optional[float] = None
-    volume_24h_max: Optional[float] = None
-    circulating_supply_min: Optional[float] = None
-    circulating_supply_max: Optional[float] = None
-    percent_change_24h_min: Optional[float] = None
-    percent_change_24h_max: Optional[float] = None
-    tags: Optional[List[str]] = None
-
-
-class AuxFields(Enum):
-    NUM_MARKET_PAIRS = "num_market_pairs"
-    CMC_RANK = "cmc_rank"
-    DATE_ADDED = "date_added"
-    TAGS = "tags"
-    PLATFORM = "platform"
-    MAX_SUPPLY = "max_supply"
-    TOTAL_SUPPLY = "total_supply"
-    MARKET_CAP_BY_TOTAL_SUPPLY = "market_cap_by_total_supply"
-    VOLUME_24H_REPORTED = "volume_24h_reported"
-    VOLUME_7D = "volume_7d"
-    VOLUME_7D_REPORTED = "volume_7d_reported"
-    VOLUME_30D = "volume_30d"
-    VOLUME_30D_REPORTED = "volume_30d_reported"
-    IS_MARKET_CAP_INCLUDED = "is_market_cap_included_in_calc"
-
-class SortDir(Enum):
-	ASC = "asc"
-	DESC = "desc"
-
-class SortOption(Enum):
-    MARKET_CAP = "market_cap"
-    MARKET_CAP_STRICT = "market_cap_strict"
-    NAME = "name"
-    SYMBOL = "symbol"
-    DATE_ADDED = "date_added"
-    PRICE = "price"
-    CIRCULATING_SUPPLY = "circulating_supply"
-    TOTAL_SUPPLY = "total_supply"
-    MAX_SUPPLY = "max_supply"
-    NUM_MARKET_PAIRS = "num_market_pairs"
-    MARKET_CAP_BY_TOTAL_SUPPLY_STRICT = "market_cap_by_total_supply_strict"
-    VOLUME_24H = "volume_24h"
-    VOLUME_7D = "volume_7d"
-    VOLUME_30D = "volume_30d"
-    PERCENT_CHANGE_1H = "percent_change_1h"
-    PERCENT_CHANGE_24H = "percent_change_24h"
-    PERCENT_CHANGE_7D = "percent_change_7d"
-
+from .v1.cryptocurrency.listings.latest import _listings_latest
+from .v1.cryptocurrency.listings.common import SortOption, AuxFields, SortDir, FilterOptions
 
 class Market(object):
 
@@ -121,54 +66,15 @@ class Market(object):
 	def quotes_historical(self, ticker: str, timestamp_start: int, timestamp_end: int, interval: str = '24h') -> List[TokenState]:
 		return _quotes_historical_v2(self, ticker, timestamp_start, timestamp_end, interval)
 
-	def quotes_historical_v2(self, ticker: str, timestamp_start: int, timestamp_end: int, interval: str = '24h') -> List[TokenState]:
-		return _quotes_historical_v2(self, ticker, timestamp_start, timestamp_end, interval)
-
-	def listings_latest(self, sort_by: SortOption = SortOption.MARKET_CAP, sort_dir: SortDir = SortDir.DESC, start: int = 1, limit: int = 100, convert: str = None, aux_fields: AuxFields = None, filters: FilterOptions = None) -> List[TokenState]:
+	def listings_latest(self, sort_by: SortOption = SortOption.MARKET_CAP, 
+					sort_dir: SortDir = SortDir.DESC, 
+					start: int = 1, 
+					limit: int = 100, 
+					convert: str = None, 
+					aux_fields: AuxFields = None, 
+					filters: FilterOptions = None) -> List[TokenState]:
 		
-		params = {
-			'sort': sort_by.value,
-			'sort_dir': sort_dir.value,
-			'start': start,
-			'limit': limit
-		}
-		
-		if convert:
-			params['convert'] = convert
-
-		if aux_fields:
-			# Include the "aux" fields in the params
-			aux_field_values = [field.value for field in aux_fields]
-			params['aux'] = ','.join(aux_field_values)
-
-		if filters:
-			if filters.price_min is not None:
-				params['price_min'] = filters.price_min
-			if filters.price_max is not None:
-				params['price_max'] = filters.price_max
-			if filters.market_cap_min is not None:
-				params['market_cap_min'] = filters.market_cap_min
-			if filters.market_cap_max is not None:
-				params['market_cap_max'] = filters.market_cap_max
-			if filters.volume_24h_min is not None:
-				params['volume_24h_min'] = filters.volume_24h_min
-			if filters.volume_24h_max is not None:
-				params['volume_24h_max'] = filters.volume_24h_max
-			if filters.circulating_supply_min is not None:
-				params['circulating_supply_min'] = filters.circulating_supply_min
-			if filters.circulating_supply_max is not None:
-				params['circulating_supply_max'] = filters.circulating_supply_max
-			if filters.percent_change_24h_min is not None:
-				params['percent_change_24h_min'] = filters.percent_change_24h_min
-			if filters.percent_change_24h_max is not None:
-				params['percent_change_24h_max'] = filters.percent_change_24h_max
-			if filters.tags:
-				params['tag'] = ','.join(filters.tags)
-
-		response = self._request('v1/cryptocurrency/listings/latest', params=params)
-		token_states = [TokenState.from_dict(token) for token in response['data']]
-
-		return token_states
+		return _listings_latest(self, sort_by, sort_dir, start, limit, convert, aux_fields, filters)
 
 
 	# TODO - this should call global metrics endpoint
