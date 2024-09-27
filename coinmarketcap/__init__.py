674e1c6ba5fdd03299951f9ffaa3aea070c5c29b
Jason Hudgins <jason@hudgins.co>
2024-09-27 10:39:04 -0500

    fixed broken import

--- coinmarketcap/__init__.py
+++ coinmarketcap/__init__.py
@@ -8,6 +8,7 @@ __repo__ = 'zhttps://github.com/jmazzahacks/byteforge-coinmarketcap'
 __license__ = 'Apache v2.0 License'
 
 from .core import Market
+from .core import ServerException
 from .core import SortOption
 from .core import SortDir
 from .core import FilterOptions
