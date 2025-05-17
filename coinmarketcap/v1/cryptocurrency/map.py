d50808ab3d070fe05c66ed939a1a218eec72add3
Jason Byteforge <jason@mzmail.me>
2025-05-17 17:09:18 +0300

    added all params to the map call

--- coinmarketcap/v1/cryptocurrency/map.py
+++ coinmarketcap/v1/cryptocurrency/map.py
@@ -12,12 +12,20 @@ class MapSortOption(Enum):
     ID = "id"
     CMC_RANK = "cmc_rank"
 
+# "platform,first_historical_data,last_historical_data,is_active"
+class MapAuxFields(Enum):
+    PLATFORM = "platform"
+    FIRST_HISTORICAL_DATA = "first_historical_data"
+    LAST_HISTORICAL_DATA = "last_historical_data"
+    IS_ACTIVE = "is_active"
+
 def _map(market, 
          status: ListingStatus = ListingStatus.ACTIVE, 
          start: int = 1,
          limit: int = 100,
          symbols: List[str] = None,
-         sort: MapSortOption = MapSortOption.ID):
+         sort: MapSortOption = MapSortOption.ID, 
+         aux_fields: List[MapAuxFields] = None):
 
     # quick and dirty test
     params = dict()
@@ -27,6 +35,8 @@ def _map(market,
     params['sort'] = sort.value
     if symbols:
         params['symbol'] = ','.join(symbols)
+    if aux_fields:
+        params['aux'] = ','.join([field.value for field in aux_fields])
 
     response = market._request('v1/cryptocurrency/map', params=params)
    
